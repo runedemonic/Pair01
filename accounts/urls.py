6dfccead81fcded51b149e61f9d31b55bfab2f44
runedemonic <runedemonic@naver.com>
2022-10-14 11:17:52 +0900

    make user info

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,6 @@ app_name = "accounts"
 urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("login/", views.login, name="login"),
+    path("index/", views.index, name="index"),
+    path("<int:pk>/detail", views.detail, name="detail"),
 ]
